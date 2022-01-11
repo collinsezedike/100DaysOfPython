c204678310781c73bcb51e87627aa46784ffb477
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2022-01-11 23:59:30 +0100

    A life in weeks program
    Calculate the how much months, weeks and days you have left taking life expectancy to be 90 years aftering subtracting how many years you have live already. 
    
    Reference: https://waitbutwhy.com/2014/05/life-weeks.html

---
+++ Day2/lifeinweeks.py
@@ -0,0 +1,9 @@
+age = input('What is your current age? ')
+
+years_left = 90 - int(age)
+
+days_left = years_left * 365
+weeks_left = years_left * 52
+months_left = years_left * 12
+
+print(f'You have {days_left} days, {weeks_left} weeks and {months_left} months left')
