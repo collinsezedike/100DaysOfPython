67fcc090b6c060449ee5d05a84869fee2f25719d
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2022-01-12 01:53:28 +0100

    Variables
    Playing around with variables.

---
+++ variables.py
@@ -0,0 +1,18 @@
+# name = input('What is your name? ')
+# print(name)
+
+# name = input('What is your name? ')
+# length = len(name)
+# print(length)
+
+# coding challenge
+
+a = input("a: ")
+b = input('b: ')
+
+c = a
+a = b
+b = c
+
+print('a = ' + a)
+print('b = ' + b)
