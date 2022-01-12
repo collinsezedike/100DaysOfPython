ed095fa00b0c97e863a08841c82880356040872e
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2022-01-12 02:15:14 +0100

    A BMI calculator

--- Day2/BMI.py
+++ Day2/BMI.py
@@ -2,4 +2,4 @@ height = input('enter your height in m: ')
 weight = input('enter your weight in kg: ')
 
 BMI = float(weight) / float(height) ** 2
-print(int(BMI))
+print(f'Your BMI is {int(BMI)}kg per square meters')
