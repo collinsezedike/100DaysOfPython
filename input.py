70350c52747b769bbf754e92462391d2d2be5bc0
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2022-01-12 01:52:06 +0100

    Input statements
    Playing around with the input syntax

---
+++ input.py
@@ -0,0 +1,9 @@
+# I'm not sure if I should create a new file called 'comment.py'
+
+
+print("Hi " + input('What is your name?\n'))
+
+
+# a program to count the letters in a name
+
+print(len(input('What is your name? ')))
