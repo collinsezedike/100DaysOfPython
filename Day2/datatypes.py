f0ac13be711309a010842fd077b956f6040f2b0b
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2022-01-12 02:04:08 +0100

    Add files via upload

---
+++ Day2/datatypes.py
@@ -0,0 +1,54 @@
+# strings - mere text,
+print('Angela')
+
+#  must be surrounded by "" or ''
+#  numbers surrounded by quotation marks are treated as strings
+print('123' + '345')
+
+# string subscripting
+print('Hello'[4])
+
+# subscripting is using [] to get an individual character in a string
+# note: programmers count from 0 not 1
+# so when you tell a programmer that they are the number 1 person in your life,
+# they get uncomfortable
+
+# Angela likes to think of strings as a chain of some sort or a string of beads
+# is it for this reason that strings can be subscripted
+# it is like taking out a single bead from the bracelet 
+
+
+# integers - whole numbers, positive or negative
+print(123_456_789)
+
+
+# float (floating point numbers) - decimal numbers
+print(3.14)
+
+# Angela explains that the fluidity of the decimal point 
+# causing it to be able to 'float' to any part of a digit
+# is the reason for the name "floating point numbers"
+
+
+# booleans - True or False
+is_dead = False
+grown_up = True
+
+
+# code challenge
+two_digit_number = input("Type a two digit number: ")
+sum_of_two_digits = int(two_digit_number[0]) + int(two_digit_number[1])
+print(sum_of_two_digits)
+
+
+# PEMDASLR
+# parenthesis
+# exponential
+# multiplication
+# division
+# addition
+# subtraction
+# left to right (added by Angela as a guide)
+
+print(3 * (3 + 3) / 3 - 3)
+
