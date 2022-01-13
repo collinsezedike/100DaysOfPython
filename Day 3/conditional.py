7648cfc2b59601697935f14b28c22e3779ed5e8b
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2022-01-13 01:40:23 +0100

    Create conditional.py

---
+++ Day 3/conditional.py
@@ -0,0 +1,7 @@
+print('Welcome to the rollercoaster ride!')
+height = int(input('What is your height in cm? '))
+
+if height >= 120:
+    print('You are tall enough for this ride.')
+else:
+    print('Sorry! You are not tall enough for this ride.')
