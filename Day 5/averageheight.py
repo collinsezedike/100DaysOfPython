e76e6450c3a9303ae5a5b1ddd5978d58e972c4de
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2022-01-15 01:08:37 +0100

    Update averageheight.py

--- Day 5/averageheight.py
+++ Day 5/averageheight.py
@@ -1,7 +1,7 @@
 student_heights = input('Input a list of students heights ').split()
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
-
+print(student_heights)
 
 
 sum_of_heights = 0
