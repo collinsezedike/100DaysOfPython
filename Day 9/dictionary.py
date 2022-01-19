ed767a65df2bee07e744c44a90d1ac9a84b0498e
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2022-01-19 23:07:08 +0100

    Create dictionary.py

---
+++ Day 9/dictionary.py
@@ -0,0 +1,28 @@
+programming_dictionary = {
+    'Bug':'An error in a program that prevents the program from running as expected.',
+    'Function':'A piece of code that you can easily call over and over again.',
+}
+
+# Retrieving from dictionary
+print(programming_dictionary['Bug'])
+
+# Adding new items to dictionary
+programming_dictionary['Loop'] = "The action of doing something over and over again."
+print(programming_dictionary)
+
+# Creatte an empty dictionary
+empty_dictionary = {}
+print(empty_dictionary)
+
+# Wipe out a dictionary
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+# Editing items to dictionary
+programming_dictionary['Bug'] = "A moth in your computer."
+print(programming_dictionary)
+
+# Looping through a dictionary
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
