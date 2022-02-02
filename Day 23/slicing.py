54c45c0137b174af8e3731eb141b2f7b3f4d2cec
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2022-02-02 01:13:34 +0100

    Add files via upload

---
+++ Day 23/slicing.py
@@ -0,0 +1,20 @@
+piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
+piano_tuples = ("do", "re", "mi", "fa", "so", "la", "ti")
+
+# slicing a list
+print(piano_keys[2:5])
+
+# slicing to the end 
+print(piano_keys[2:])
+
+# slicing from beginning
+print(piano_keys[:5])
+
+# slicing with a step
+print(piano_keys[::2])
+
+# reverse a list with slicing
+print(piano_keys[::-1])
+
+# slicing a tuple
+print(piano_tuples[2:5])
