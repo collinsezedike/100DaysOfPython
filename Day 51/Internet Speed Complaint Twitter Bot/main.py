a933a3331227b5f2dfbf023cf191bd660a59c738
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2022-03-09 15:51:49 +0100

    Update main.py

--- Day 51/Internet Speed Complaint Twitter Bot/main.py
+++ Day 51/Internet Speed Complaint Twitter Bot/main.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 chrome_driver_path = "../../chromedriver.exe"
@@ -13,15 +17,23 @@ INTERNET_PROVIDER = "MTNNG"
 class InternetSpeedTwitterBot:
 
     def __init__(self):
-        self.driver = webdriver.Chrome(service=service_obj)
+        # configure pageLoadStrategy to none
+        c = DesiredCapabilities.CHROME
+        c["pageLoadStrategy"] = "none"
+        self.driver = webdriver.Chrome(service=service_obj, desired_capabilities=c)
         self.download_speed = None
         self.upload_speed = None
 
     def get_internet_speed(self):
+        w = WebDriverWait(self.driver, 30)
         self.driver.get("https://www.speedtest.net/")
-        time.sleep(10)
-        self.driver.find_element(By.CLASS_NAME, value="js-start-test").click()
-        time.sleep(45)
+        # wait till go button shows up
+        w.until(EC.element_to_be_clickable((By.CLASS_NAME, "js-start-test")))
+        # JavaScript Executor to stop page load
+        time.sleep(20)
+        self.driver.execute_script("window.stop();")
+        self.driver.find_element(By.CLASS_NAME, value="js-start-test").click()  # click the go button
+        time.sleep(60)
         self.download_speed = float(self.driver.find_element(By.CSS_SELECTOR, value="span.download-speed").text)
         self.upload_speed = float(self.driver.find_element(By.CSS_SELECTOR, value="span.upload-speed").text)
         print(f"Your download speed is {self.download_speed}Mbps"
@@ -33,27 +45,26 @@ class InternetSpeedTwitterBot:
         self.driver.quit()
 
     def tweet_at_provider(self):
-        self.driver.switch_to.new_window("tab")
         self.driver.get("https://twitter.com/i/flow/login")
-        time.sleep(5)
-        phone_number_input = self.driver.find_element(By.NAME, value="text")
-        phone_number_input.send_keys("email address")
-        self.driver.find_element(
-            By.XPATH,
-            value='//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[6]/div/span/span').click()
+        time.sleep(20)
+        username_input = self.driver.find_element(By.NAME, value="text")
+        username_input.send_keys("My username")
+        username_input.send_keys(Keys.ENTER)
         time.sleep(3)
         password_input = self.driver.find_element(By.NAME, value="password")
-        password_input.send_keys("password")
-        self.driver.find_element(
-            By.XPATH,
-            value='//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/span/span').click()
+        password_input.send_keys("My password")
+        password_input.send_keys(Keys.ENTER)
         time.sleep(25)
         tweet_textarea = self.driver.find_element(By.CLASS_NAME, value="public-DraftStyleDefault-block")
         tweet_textarea.click()
         tweet_textarea.send_keys(f"Hey @{INTERNET_PROVIDER}, why is my internet speed {self.download_speed}down/{self.upload_speed}"
                                  f"up when I pay for {SUPPOSED_SPEED}Mbps?")
         time.sleep(2)
-        self.driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div/div/span/span').click()
+        # tweet button
+        self.driver.find_element(
+            By.XPATH,
+            value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div/div/span/span').click()
+        print("Tweet sent!")
 
 
 bot = InternetSpeedTwitterBot()
