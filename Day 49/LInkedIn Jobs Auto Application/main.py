f9e7199acf1c85629b9e560d0f819b0387fe7775
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2022-03-09 13:14:32 +0100

    Update main.py

--- Day 49/LInkedIn Jobs Auto Application/main.py
+++ Day 49/LInkedIn Jobs Auto Application/main.py
@@ -11,14 +11,15 @@ driver = webdriver.Chrome(service=service_obj)
 driver.get("https://www.linkedin.com/jobs/search/?currentJobId=2923865231&f_LF=f_AL&geoId=102257491&"
            "keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom")
 
-driver.find_element(By.XPATH, value="/html/body/div[1]/header/nav/div/a[2]").click()
+driver.find_element(By.XPATH, value="/html/body/div[1]/header/nav/div/a[2]").click()               # sign in button
 time.sleep(5)
-username = driver.find_element(By.ID, value="username")
-username.send_keys("Username")
-password = driver.find_element(By.ID, value="password")
-password.send_keys("Password")
-driver.find_element(By.XPATH, value='//*[@id="organic-div"]/form/div[3]/button').click()
+username_input = driver.find_element(By.ID, value="username")
+username_input.send_keys("My username")
+password_input = driver.find_element(By.ID, value="password")
+password_input.send_keys("My password")
+driver.find_element(By.XPATH, value='//*[@id="organic-div"]/form/div[3]/button').click()           # sign in button
 time.sleep(5)
+
 gigs = driver.find_elements(By.CLASS_NAME, value="job-card-list__title")
 for gig in gigs:
     gig.click()
@@ -26,11 +27,13 @@ for gig in gigs:
     try:
         driver.find_element(By.CLASS_NAME, value="jobs-apply-button").click()
         apply_button = driver.find_element(By.CSS_SELECTOR, value="footer button")
-        if apply_button.get_attribute("aria-label") == "Submit application":
+        # if the application does not require any input, go through with it, 
+        # else quit applying and move on to the next application
+        if apply_button.get_attribute("aria-label") == "Submit application":             
             apply_button.click()
         else:
-            driver.find_element(By.CLASS_NAME, value="artdeco-modal__dismiss").click()
+            driver.find_element(By.CLASS_NAME, value="artdeco-modal__dismiss").click()             # the x button
             time.sleep(2)
-            driver.find_element(By.CLASS_NAME, value="artdeco-button--secondary").click()
+            driver.find_element(By.CLASS_NAME, value="artdeco-button--secondary").click()          # confirm application dismissal
     except NoSuchElementException:
         print("No application found")
