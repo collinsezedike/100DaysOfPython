1eed3692fbc3390e63e17f88a4eaac7049443f18
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2022-03-14 14:36:00 +0100

    Update workingwithemailsmtp.py

--- Day 32/workingwithemailsmtp.py
+++ Day 32/workingwithemailsmtp.py
@@ -4,12 +4,11 @@ my_email = "sender@gmail.com"
 my_password = "sender password"
 recipient_email = "recipient@email.com"
 
-# connection = smtplib.SMTP("smtp.gmail.com")
-# connection.starttls()
-# connection.login(user=my_email, password=my_password)
-# connection.sendmail(
-#     from_addr=my_email,
-#     to_addrs=recipient_email,
-#     msg="Subject:Hello\n\nThis is the content of the message")
-# connection.close()
-
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email, password=my_password)
+connection.sendmail(
+    from_addr=my_email,
+    to_addrs=recipient_email,
+    msg="Subject:Hello\n\nThis is the content of the message")
+connection.close()
