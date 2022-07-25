cda59ca72ba7ff0a945fa76b484d07a7a8a3ccbe
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2022-07-25 02:27:10 +0100

    A little improvement on User Experience

--- Day 90/Professional Project - Text to Speech (Converting PDFs to Audiobooks)/main.py
+++ Day 90/Professional Project - Text to Speech (Converting PDFs to Audiobooks)/main.py
@@ -1,11 +1,14 @@
 import pyttsx3
 import pdfplumber
 
+print("Reading PDF file...")
 with pdfplumber.open("file.pdf") as pdf:
     all_content = ""
     for page in pdf.pages:
         all_content+=page.extract_text()
 
+print("Generating audio...")
 engine = pyttsx3.init()
 engine.save_to_file(all_content, 'audio.mp3')
+print("Audio file generated")
 engine.runAndWait()
